84d3ea18f22fd8f6eb7340573bf737148b47f287
302A-01\cre <a>
2025-06-17 16:06:32 +0900

    kadai1

--- src/Kadai1.java
+++ src/Kadai1.java
@@ -1,13 +1,13 @@
 public class Kadai1 {
     public static void main(String[] args) {
+        int i1 = Integer.parseInt(args[0]);
         for (int i = 0; i < 1000; i++){
             int num = i + 1;
             System.out.print(num);
             System.out.print(" ");
-            if (num % 10 == 0) {
+            if (num % i1 == 0) {
                 System.out.println();
             }
-            // commit test
         }
     }
 }
