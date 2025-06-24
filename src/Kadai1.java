aacdb90ac68d7f795f1e3fbf25ba7dc9e8abe0d6
302A-01\cre <a>
2025-06-24 13:48:25 +0900

    kadai 1　終わり

--- src/Kadai1.java
+++ src/Kadai1.java
@@ -1,13 +1,19 @@
 public class Kadai1 {
     public static void main(String[] args) {
-        int i1 = Integer.parseInt(args[0]);
-        for (int i = 0; i < 1000; i++){
-            int num = i + 1;
-            System.out.print(num);
-            System.out.print(" ");
-            if (num % i1 == 0) {
-                System.out.println();
+        try {
+            int i1 = Integer.parseInt(args[0]);
+            for (int i = 0; i < 1000; i++){
+                int num = i + 1;
+                System.out.print(num);
+                System.out.print(" ");
+                if (num % i1 == 0) {
+                    System.out.println();
+                }
             }
+        } catch (NumberFormatException e) {
+            System.out.println("数字以外が入力されています");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("何も入力されていません");
         }
     }
 }
