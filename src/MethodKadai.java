cfca4cf4bb4282710bc9a41562971125db931540
302A-01\cre <a>
2025-07-15 16:08:01 +0900

    kadai 3 start

--- src/MethodKadai.java
+++ src/MethodKadai.java
@@ -5,27 +5,32 @@ public class MethodKadai {
         // int result = sum(1,4);
         // System.out.println(sum(1,4));
         //greet("なかた");
-        int num = (2);
+       //int result = doubleNumber(10,2);
+       //System.out.println(doubleNumber(10,2));
+        boolean result = isEven()
     }
+
 
     // 引数なし 戻り値なし
-    public static void hello() {
-        System.out.println("こんにちは");
-    }
+    //public static void hello() {
+      //  System.out.println("こんにちは");
+    //}
 
     // 引数あり 戻り値なし
-    public static void callName(String name) {
-        System.out.println(name);
-    }
+    //public static void callName(String name) {
+      //  System.out.println(name);
+    //}
 
     // 引数あり 戻り値あり
-    public static int sum(int x, int y) {
-        return x + y;
-    }
-    public static void greet(String name) {
-        System.out.println("こんにちは、" + name + "さん！");
-    }
-    public static int doubleNumber (int num) {
-        return num * 2;
-    }
+    //public static int sum(int x, int y) {
+    //  return x + y;
+    //}
+    //public static void greet(String name) {
+    //  System.out.println("こんにちは、" + name + "さん！");
+    //}
+    //public static int doubleNumber (int x, int y) {
+    //    return x * y;
+      public static boolean isEven (int x, int y) {
+        return x % y;
+      }
 }
